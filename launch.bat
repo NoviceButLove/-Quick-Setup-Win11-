7aa4cdad23acd09d8052daee8cf7a9610e6c8ea9
novicebutlove <2912154279@qq.com>
2024-05-31 16:36:07 +0800

    Update launch.bat
    This file is the file I use now, without any
    problem.
    
    Open VScode or Powershell which have their
    own terminal or background program will
    occupied command prompt(命令提示符) so that
    terminal can't close properly.

--- launch.bat
+++ launch.bat
@@ -11,21 +11,30 @@ set "path=D:\$ROOT\ShortCut\"
 
 
 rem Set the variable "command" to the path of the shortcut
-set "command=%path%wechat.lnk"
+set "command=%path%pixpin.lnk"
 rem start in background, still, use "start", meaning start independently
-start "" %command%
-
-set "command=%path%code.lnk"
-start "" %command%
-
-set "command=%path%clash.lnk"
 start "" %command%
 
 set "command=%path%caps.lnk"
 start "" %command%
 
-set "command=%path%ps.lnk"
+set "command=%path%akey.lnk"
 start "" %command%
+
+rem "command=%path%ps.lnk"
+rem start "" %command%
+
+rem set "command=%path%powertoy.lnk"
+rem start "" /B %command%
+
+set "command=%path%clash.lnk"
+start "" /B %command%
+
+set "command=%path%wireguard.lnk"
+start "" /B %command%
+
+rem set "command=%path%QuickLook.lnk"
+rem start "" /B %command%
 
 set "command=%path%edge.lnk"
 start "" %command%
@@ -36,8 +45,25 @@ start "" %command%
 set "command=%path%lock.lnk"
 start "" %command%
 
+set "command=%path%ebook.lnk"
+start "" %command%
+
 set "command=%path%pdfreader.lnk"
 start "" %command%
+
+rem set "command=%path%throttle.lnk"
+rem start "" %command%
+
+
+
+
+rem Special setinngs
+
+rem set "path=C:\Program Files\PowerToys\"  
+rem start "" %command%
+
+rem set "command=%path%PowerToys.exe"
+
 
 rem Exit in force
 exit
